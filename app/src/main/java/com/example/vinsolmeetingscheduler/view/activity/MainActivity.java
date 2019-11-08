0ab78434a5a65f4acbd8598fb101cd49eca9eddb
Apoorv1180 <apoorv111221cse@gmail.com>
2019-11-09 00:27:13 +0530

    Project bug fixed and Ui refactored

--- app/src/main/java/com/example/vinsolmeetingscheduler/view/activity/MainActivity.java
+++ app/src/main/java/com/example/vinsolmeetingscheduler/view/activity/MainActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.vinsolmeetingscheduler.R;
@@ -28,16 +29,22 @@ import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    //view
     LinearLayout previous_button_click, forward_button_click;
     RecyclerView recyclerView;
     LinearLayoutManager layoutManager;
-    MeetingAdapter adapter;
-    ScheduleMeetingViewModel scheduleMeetingViewModel;
-    ArrayList<ScheduleMeetingResponse> schedulelist = new ArrayList<>();
-    Date date,decrementDate,incrementDate;
     Button btnScheduleMeeting;
     TextView emptyTextView;
+    ProgressBar progressBar;
+    TextView title_text;
+    MeetingAdapter adapter;
+
+    //view model
+    ScheduleMeetingViewModel scheduleMeetingViewModel;
+
+    //variables
+    ArrayList<ScheduleMeetingResponse> schedulelist = new ArrayList<>();
+    Date date, decrementDate, incrementDate;
 
 
     @Override
@@ -45,28 +52,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        date = new Date();
-        Constant.saveDate(this,Constant.convertDateToString(date));
-
+        getDate();
         initToolBar();
         initListeners();
         initRecyclerView();
         initViewModel();
+    }
 
-
-
+    private void getDate() {
+        date = new Date();
+        Constant.saveDate(this, Constant.convertDateToString(date));
     }
 
     private void initView() {
         btnScheduleMeeting = findViewById(R.id.schedule_meeting_button);
-        emptyTextView=findViewById(R.id.emptyView);
+        emptyTextView = findViewById(R.id.emptyView);
+        progressBar = findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.VISIBLE);
     }
 
     private void initRecyclerView() {
         recyclerView = findViewById(R.id.schedule_list_recycler_view);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new MeetingAdapter(schedulelist,this);
+        adapter = new MeetingAdapter(schedulelist, this);
         recyclerView.setHasFixedSize(true);
         recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
         recyclerView.setAdapter(adapter);
@@ -74,26 +83,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initViewModel() {
         scheduleMeetingViewModel = ViewModelProviders.of(this).get(ScheduleMeetingViewModel.class);
-        observeViewModel(scheduleMeetingViewModel,Constant.getDate(this));
+        observeViewModel(scheduleMeetingViewModel, Constant.getDate(this));
     }
 
     private void observeViewModel(ScheduleMeetingViewModel scheduleMeetingViewModel, String date) {
         scheduleMeetingViewModel.getMeetingResponse(date).observe(this, new Observer<List<ScheduleMeetingResponse>>() {
             @Override
             public void onChanged(List<ScheduleMeetingResponse> scheduleMeetingResponses) {
-                if(scheduleMeetingResponses!=null && scheduleMeetingResponses.size()!=0){
+                if (scheduleMeetingResponses != null && scheduleMeetingResponses.size() != 0) {
                     schedulelist.clear();
                     schedulelist.addAll(scheduleMeetingResponses);
                     adapter.notifyDataSetChanged();
                     emptyTextView.setVisibility(View.GONE);
                     recyclerView.setVisibility(View.VISIBLE);
-                }
-                else{
-                  emptyTextView.setVisibility(View.VISIBLE);
-recyclerView.setVisibility(View.GONE);
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    emptyTextView.setVisibility(View.VISIBLE);
+                    recyclerView.setVisibility(View.GONE);
+                    progressBar.setVisibility(View.GONE);
                 }
             }
         });
+
     }
 
     private void initToolBar() {
@@ -103,34 +114,36 @@ recyclerView.setVisibility(View.GONE);
         View view = getSupportActionBar().getCustomView();
         previous_button_click = view.findViewById(R.id.back_click);
         forward_button_click = view.findViewById(R.id.forward_click);
+        title_text = view.findViewById(R.id.title_text);
+        title_text.setText(Constant.convertDateToString(date));
     }
+
     private void initListeners() {
         previous_button_click.setOnClickListener(this);
         forward_button_click.setOnClickListener(this);
         btnScheduleMeeting.setOnClickListener(this);
 
     }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.back_click:
-                Log.e("TAG", "BACK");
+                progressBar.setVisibility(View.VISIBLE);
                 decrementDate = Constant.decrementAdataByOneDay(Constant.convertStringToDate(Constant.getDate(this)));
-                Constant.saveDate(this,Constant.convertDateToString(decrementDate));
-                Log.e("TAG", decrementDate.toString());
-                observeViewModel(scheduleMeetingViewModel,Constant.getDate(this));
+                Constant.saveDate(this, Constant.convertDateToString(decrementDate));
+                observeViewModel(scheduleMeetingViewModel, Constant.getDate(this));
 
                 break;
             case R.id.forward_click:
-                Log.e("TAG", "BACK");
+                progressBar.setVisibility(View.VISIBLE);
                 incrementDate = Constant.incerementAdateByOneDay(Constant.convertStringToDate(Constant.getDate(this)));
-                Constant.saveDate(this,Constant.convertDateToString(incrementDate));
-                Log.e("TAG", incrementDate.toString());
-                observeViewModel(scheduleMeetingViewModel,Constant.getDate(this));
+                Constant.saveDate(this, Constant.convertDateToString(incrementDate));
+                observeViewModel(scheduleMeetingViewModel, Constant.getDate(this));
                 break;
             case R.id.schedule_meeting_button:
-                Intent intent = new Intent(this,ScheduleMeetingFormActivity.class);
-                intent.putExtra("date",Constant.getDate(this));
+                Intent intent = new Intent(this, ScheduleMeetingFormActivity.class);
+                intent.putExtra(getString(R.string.date_intent_extra), Constant.getDate(this));
                 startActivity(intent);
         }
     }
